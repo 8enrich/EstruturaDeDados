ba36f8e08003ff066a2a82a813e25cba25c1dc52
8enrich <henriqueftcunha@gmail.com>
2025-01-16 13:55:15 -0300

    Remove da árvore avl

--- binary_tree/avl.py
+++ binary_tree/avl.py
@@ -6,13 +6,20 @@ class Avl(BinTree):
         super(Avl, self).__init__(root)
 
     def __find_deepest_disbalanced(self, node):
-        if not node:
-            return node 
         if self.__is_disbalanced(node.left):
-            return self.__find_deepest_disbalanced(node.left)
+            node.left = self.__find_deepest_disbalanced(node.left)
+            return node
         if self.__is_disbalanced(node.right):
-            return self.__find_deepest_disbalanced(node.right)
-        return node 
+            node.right = self.__find_deepest_disbalanced(node.right)
+            return node
+        return self.__do_rotate(node) 
+
+    def __do_rotate(self, z):
+        y = self.__find_highest_son(z)
+        x = self.__find_highest_son(y)
+        if((z.left == y and y.left == x) or (z.right == y and y.right == x)):
+            return self.__single_rotation(x, y, z)
+        return self.__double_rotation(x, y, z)
 
     def __find_highest_son(self, node):
         if not node:
@@ -32,25 +39,42 @@ class Avl(BinTree):
         z.right = t2 
         return y
 
+    def __left_right(self, x, y, z):
+        t2, t3 = x.left, x.right
+        x.left, x.right = y, z
+        y.right = t2
+        z.left = t3
+        return x
+
+    def __right_left(self, x, y, z):
+        t2, t3 = x.left, x.right
+        x.left, x.right = z, y
+        z.right = t2
+        y.left = t3
+        return x
+
     def __single_rotation(self, x, y, z):
-        if self.root == z:
-            self.root = y
         if z.left == y:
             return self.__left_left(x, y, z)
         return self.__right_right(x, y, z)
 
-    def __rotate(self, desbalanced):
-        z = self.__find_deepest_disbalanced(self.root)
-        y = self.__find_highest_son(z)
-        x = self.__find_highest_son(y)
-        if((z.left == y and y.left == x) or (z.right == y and y.right == x)):
-            return self.__single_rotation(x, y, z)
+    def __double_rotation(self, x, y, z):
+        if z.left == y:
+            return self.__left_right(x, y, z)
+        return self.__right_left(x, y, z)
+
+    def __rotate(self):
+        self.root = self.__find_deepest_disbalanced(self.root)
 
     def insert(self, value):
         super().insert(value)
-        desbalanced = self.is_disbalanced()
-        if desbalanced:
-            self.__rotate(desbalanced) 
+        if self.__is_disbalanced(self.root):
+            self.__rotate() 
+
+    def remove(self, value):
+        super().remove(value)
+        while self.__is_disbalanced(self.root):
+            self.__rotate()
             
     def __get_heights(self, node):
         r_height = node.right.get_height() if node.right else 0
@@ -65,12 +89,5 @@ class Avl(BinTree):
             return r_height - l_height
         return self.__is_disbalanced(node.left) + self.__is_disbalanced(node.right)
 
-    def is_disbalanced(self):
+    def is_desbalanced(self):
         return self.__is_disbalanced(self.root)
-
-avl = Avl()
-for i in range(7):
-    avl.insert(i + 1)
-    print(avl)
-    print()
-print(avl)
